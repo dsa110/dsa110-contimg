0aa2026f17dd8a2054093d31d01e3541ea3086d5
jakobtfaber <jakobtfaber@users.noreply.github.com>
2025-11-29 15:30:58 +0000

    deploy: 498c80579ad6c399810ceec427099b5a8095c63f

---
+++ dashboard/assets/loadEcharts-CzvmfJAx.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./index-CPQ429lP.js";let r=null;const e=async()=>(r||(r=t(()=>import("./echarts-BlxdVhSE.js"),[])),r);export{e as l};
+//# sourceMappingURL=loadEcharts-CzvmfJAx.js.map
