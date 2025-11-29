d933901d983b8075f2807c4b3f05e7509d7630f2
Jakob Faber <jakobtfaber@gmail.com>
2025-11-29 06:04:32 -0800

    fix: add missing import to example.spec.ts

--- frontend/e2e/example.spec.ts
+++ frontend/e2e/example.spec.ts
@@ -1,5 +1,7 @@
-test('hello world', async ({ page }) => {
-    await page.goto('https://example.com');
-    const title = await page.title();
-    expect(title).toBe('Example Domain');
+import { test, expect } from "@playwright/test";
+
+test("hello world", async ({ page }) => {
+  await page.goto("https://example.com");
+  const title = await page.title();
+  expect(title).toBe("Example Domain");
 });
