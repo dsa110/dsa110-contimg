00a122136fa009b70fd4f6f700f834419cf2f850
jakobtfaber <jakobtfaber@users.noreply.github.com>
2025-11-29 14:54:20 +0000

    deploy: f515c882aeb335b4081f5db46e5702f637261046

---
+++ dashboard/assets/loadEcharts-DV8Ji-bN.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./index-QIbW7jSm.js";let r=null;const e=async()=>(r||(r=t(()=>import("./echarts-BlxdVhSE.js"),[])),r);export{e as l};
+//# sourceMappingURL=loadEcharts-DV8Ji-bN.js.map
